9b6615366298fb66ff7d3d3a8f663083a2793529
Lauri Riikonen <lauri92.riikonen@gmail.com>
2023-03-17 03:37:10 +0200

    Special buttons on number rows¨

--- lib/Screens/CalculatorScreen.dart
+++ lib/Screens/CalculatorScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_calculator/CalculatorAppState.dart';
 import 'package:provider/provider.dart';
@@ -13,7 +14,6 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
     final appState = context.watch<CalculatorAppState>();
-    var colorScheme = Theme.of(context).colorScheme;
 
     return Scaffold(
       body: SafeArea(
@@ -39,9 +39,12 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                     ],
                   ),
                 ),
-                const NumberButtonRow(list: [4, 5, 6]),
-                const NumberButtonRow(list: [4, 5, 6]),
-                const NumberButtonRow(list: [1, 2, 3])
+                const NumberButtonRow(
+                    list: [7, 8, 9], type: SpecialButtonType.multiplication),
+                const NumberButtonRow(
+                    list: [4, 5, 6], type: SpecialButtonType.subtraction),
+                const NumberButtonRow(
+                    list: [1, 2, 3], type: SpecialButtonType.sum)
               ],
             ),
           ),
@@ -68,7 +71,7 @@ class NumberButton extends StatelessWidget {
       },
       style: ElevatedButton.styleFrom(
         shape: const CircleBorder(),
-        padding: const EdgeInsets.all(30),
+        padding: const EdgeInsets.all(22),
       ),
       child: Text(
         title,
@@ -78,10 +81,42 @@ class NumberButton extends StatelessWidget {
   }
 }
 
+class SpecialButton extends StatelessWidget {
+  SpecialButton({super.key, required this.type}) {
+    if (type == SpecialButtonType.multiplication) {
+      icon = CupertinoIcons.multiply;
+    } else if (type == SpecialButtonType.subtraction) {
+      icon = Icons.remove;
+    } else if (type == SpecialButtonType.sum) {
+      icon = Icons.add;
+    }
+  }
+
+  final SpecialButtonType type;
+  late final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final appState = context.watch<CalculatorAppState>();
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        shape: const CircleBorder(),
+        padding: const EdgeInsets.all(22),
+      ),
+      child: Icon(
+        icon,
+        size: 22,
+      ),
+    );
+  }
+}
+
 class NumberButtonRow extends StatelessWidget {
   final List<int> list;
+  final SpecialButtonType type;
 
-  const NumberButtonRow({super.key, required this.list});
+  const NumberButtonRow({super.key, required this.list, required this.type});
 
   @override
   Widget build(BuildContext context) {
@@ -96,9 +131,15 @@ class NumberButtonRow extends StatelessWidget {
             NumberButton(number: list[1]),
             const SizedBox(width: 15),
             NumberButton(number: list[2]),
+            const SizedBox(width: 15),
+            SpecialButton(
+              type: type,
+            )
           ],
         ),
       ),
     );
   }
 }
+
+enum SpecialButtonType { multiplication, subtraction, sum }
